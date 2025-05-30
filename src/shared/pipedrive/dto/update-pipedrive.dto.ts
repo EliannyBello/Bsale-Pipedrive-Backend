1cc7eba7ce454f9b20fd7f3888770e5f3cb50cd0
EliannyBello <elibellomanzo@gmail.com>
2025-05-30 17:54:16 -0400

    code: apiKey for pipedrive

--- src/shared/pipedrive/dto/update-pipedrive.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreatePipedriveDto } from './create-pipedrive.dto';
-
-export class UpdatePipedriveDto extends PartialType(CreatePipedriveDto) {}
--- src/shared/pipedrive/pipedrive.module.ts
+++ src/shared/pipedrive/pipedrive.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PipedriveService } from './pipedrive.service';
-import { PipedriveController } from './pipedrive.controller';
+import { PipedriveToken, PipedriveTokenSchema } from './entities/pipedrive.entity';
 
 @Module({
-  controllers: [PipedriveController],
+  imports: [
+    MongooseModule.forFeature([
+      { name: PipedriveToken.name, schema: PipedriveTokenSchema },
+    ]),
+  ],
   providers: [PipedriveService],
+  exports: [PipedriveService], 
 })
 export class PipedriveModule {}
